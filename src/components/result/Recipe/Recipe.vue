d78de3eb5436f3dff3548c79833b1db13d52da09
Mathieu Griffoul <mathieu.griffoul@zenika.com>
2020-02-29 15:45:28 +0100

    css on recipes

--- src/components/result/Recipe/Recipe.vue
+++ src/components/result/Recipe/Recipe.vue
@@ -1,42 +1,82 @@
 <template>
-  <section>
-    <div class="card" style="width: 18rem;">
-      <div class="card-header">
-        {{ recipe.title }}
-      </div>
-      <img class="card-img-top" v-bind:src=recipe.image alt="Card image cap">
-      <div class="card-body">
-        <p class="card-text">
-            Some quick example
-        </p>
-      </div>
-    </div>
-  </section>
+  <div class="recipe">
+        <div class="recipe-image">
+          <img v-bind:src=this.recipe.image alt="Card image cap">
+        </div>
+        <div class="recipe-body">
+          <span class="recipe-title">
+            {{ recipe.title }}
+          </span>
+          <div class="heart mr-auto">
+            <img class="heart-icon" src="@/assets/heart-fill.svg">
+            <span class="likes">{{ recipe.likes }} likes</span>
+          </div>
+          <span class="recipe-text">
+            <span class="recipe-text-title"> Ingredients : </span>
+            <span class="recipe-ingredient"
+            v-for="missedIngredients in recipe.missedIngredients" :key="missedIngredients">
+              {{ missedIngredients.name}},
+            </span>
+            <span class="recipe-ingredient"
+            v-for="usedIngredient in recipe.usedIngredients" :key="usedIngredient">
+              {{ usedIngredient.name}},
+            </span>
+            <span class="recipe-ingredient"
+            v-for="unusedIngredient in recipe.unusedIngredients" :key="unusedIngredient">
+              {{ unusedIngredient.name}},
+            </span>
+          </span>
+        </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
 
 export default {
   name: 'Recipe',
-  computed: {
-    ...mapGetters([
-      'getRecipes',
-    ]),
+  props: {
+    recipe: Object,
   },
 };
 </script>
 
 <style scoped lang="scss">
-#recipes-list{
-  list-style: none;
-  padding-left: 0;
-  ul {
-  list-style: none;
-}
-  li {
-    margin-top: $primary-space;
+.recipe {
+    display: flex;
+    .recipe-body {
+      margin: 0 $left-space-medium;
+      display: flex;
+      flex-flow: column;
+      .recipe-title {
+        border-bottom: 1px solid $shadow-grey;
+        font-weight: 700;
+      }
+      .heart {
+        display: flex;
+        line-height: 1;
+        align-items: center;
+        margin-top: $primary-space;
+        .heart-icon {
+          filter: $orange-primary;
+          width: 1.2rem;
+          height: 1.2rem;
+        }
+        span {
+          margin-left: $left-space-small;
+          color: #808080;
+        }
+      }
+      .recipe-text {
+        margin-top: $primary-space;
+        margin-right: auto;
+        .recipe-text-title {
+          font-size: 0.9rem;
+          font-weight: 600;
+          color: #515151;
+        }
+        .recipe-ingredient {
+          font-size: 0.8rem;
+        }
+      }
+    }
   }
-
-}
 </style>
--- src/components/result/recipeList/RecipesList.vue
+++ src/components/result/recipeList/RecipesList.vue
@@ -1,15 +1,33 @@
 <template>
   <ul id="recipes-list">
     <li v-for="recipe in getRecipes" :key="recipe.id" class="_recipe">
-      <div class="card" style="width: 18rem;">
-        <div class="card-header">
-          {{ recipe.title }}
+      <div class="recipe">
+        <div class="recipe-image">
+          <img v-bind:src=recipe.image alt="Card image cap">
         </div>
-        <img class="card-img-top" v-bind:src=recipe.image alt="Card image cap">
-        <div class="card-body">
-          <p class="card-text">
-              Some quick example
-          </p>
+        <div class="recipe-body">
+          <span class="recipe-title">
+            {{ recipe.title }}
+          </span>
+          <div class="heart mr-auto">
+            <img class="heart-icon" src="@/assets/heart-fill.svg">
+            <span class="likes">{{ recipe.likes }} likes</span>
+          </div>
+          <span class="recipe-text">
+            <span class="recipe-text-title"> Ingredients : </span>
+            <span class="recipe-ingredient"
+            v-for="missedIngredients in recipe.missedIngredients" :key="missedIngredients">
+              {{ missedIngredients.name}},
+            </span>
+            <span class="recipe-ingredient"
+            v-for="usedIngredient in recipe.usedIngredients" :key="usedIngredient">
+              {{ usedIngredient.name}},
+            </span>
+            <span class="recipe-ingredient"
+            v-for="unusedIngredient in recipe.unusedIngredients" :key="unusedIngredient">
+              {{ unusedIngredient.name}},
+            </span>
+          </span>
         </div>
       </div>
     </li>
@@ -39,9 +57,50 @@ export default {
   list-style: none;
 
     li {
-      margin: $primary-space 0 0 $left-primary-space;
+      margin: $primary-space 0 0 $left-space-large;
       flex-basis: 100%;
     }
+
+  .recipe {
+    display: flex;
+    .recipe-body {
+      margin: 0 $left-space-medium;
+      display: flex;
+      flex-flow: column;
+      .recipe-title {
+        border-bottom: 1px solid $shadow-grey;
+        font-weight: 700;
+      }
+      .heart {
+        display: flex;
+        line-height: 1;
+        align-items: center;
+        margin-top: $primary-space;
+        .heart-icon {
+          filter: $orange-primary;
+          width: 1.2rem;
+          height: 1.2rem;
+        }
+        span {
+          margin-left: $left-space-small;
+          color: #808080;
+        }
+      }
+      .recipe-text {
+        margin-top: $primary-space;
+        margin-right: auto;
+        .recipe-text-title {
+          font-size: 0.9rem;
+          font-weight: 600;
+          color: #515151;
+        }
+        .recipe-ingredient {
+          font-size: 0.8rem;
+        }
+      }
+    }
+
+  }
 
 }
 </style>
